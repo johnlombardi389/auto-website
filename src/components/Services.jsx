e9d4066f6fbd79583bef1a5187974b38f3b4ec0a
johnlombardi389 <lombardij38@gmail.com>
2023-11-20 16:09:08 -0500

    Created and styled a services grid section

---
+++ src/components/Services.jsx
@@ -0,0 +1,42 @@
+const Services = () => {
+  return (
+    <section className="bg-gray-100 border-t border-gray-200 border-b p-8 text-center">
+      <h3 className="text-2xl font-bold mb-4">What services do we provide?</h3>
+      <p className="text-gray-700 mb-8">Explore our range of services below.</p>
+
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8">
+        {/* Service Box 1 */}
+        <div className="bg-white p-4 text-center border border-gray-300">
+          <p>Service 1</p>
+        </div>
+
+        {/* Service Box 2 */}
+        <div className="bg-white p-4 text-center border border-gray-300">
+          <p>Service 2</p>
+        </div>
+
+        {/* Service Box 3 */}
+        <div className="bg-white p-4 text-center border border-gray-300">
+          <p>Service 3</p>
+        </div>
+
+        {/* Service Box 4 */}
+        <div className="bg-white p-4 text-center border border-gray-300">
+          <p>Service 4</p>
+        </div>
+
+        {/* Service Box 5 */}
+        <div className="bg-white p-4 text-center border border-gray-300">
+          <p>Service 5</p>
+        </div>
+
+        {/* Service Box 6 */}
+        <div className="bg-white p-4 text-center border border-gray-300">
+          <p>Service 6</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 // Components
 import Intro from "../components/Intro";
+import Services from "../components/Services";
 
 const Home = () => {
   return (
     <main className="min-h-screen pt-16">
       <h1>Hello Home Page</h1>
       <Intro />
+      <Services />
     </main>
   );
 };
