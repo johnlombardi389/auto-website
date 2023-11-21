3d3c0f518c5019daa0734890c58b0fe7a3b821d9
johnlombardi389 <lombardij38@gmail.com>
2023-11-21 16:08:16 -0500

    Created reviews section with image and review quote positioned above image

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,9 +1,33 @@
+// Images
+import reviewImg from "../assets/reviewImg.jpg";
+
 const Reviews = () => {
   return (
-    <section id="reviews">
-      <h1>hello section</h1>
+    <section id="reviews" className="relative h-96 mb-0 overflow-hidden">
+      {/* Overlay Box */}
+      <div className="absolute inset-0 flex items-center justify-start p-12">
+        {/* Quote Box */}
+        <div className="bg-gray-800 bg-opacity-75 p-4 rounded-l-md w-1/3">
+          {/* Customer Quote */}
+          <blockquote className="text-sm font-italic text-left text-white">
+            "Excellent service! The team here is knowledgeable, and they always
+            go above and beyond for their customers."
+          </blockquote>
+
+          {/* Customer Name */}
+          <p className="text-sm mt-2 font-bold text-white">- Jane Doe</p>
+        </div>
+      </div>
+
+      {/* Background Image */}
+      <div
+        className="w-full h-full bg-cover bg-center"
+        style={{ backgroundImage: `url(${reviewImg})` }}
+      />
     </section>
   );
 };
 
 export default Reviews;
+
+// image Photo by Antoni Shkraba: https://www.pexels.com/photo/smiling-woman-sitting-inside-the-car-7144214/
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,7 @@ const Home = () => {
       <Intro />
       <Services />
       <Images />
+      <Reviews />
       <FAQ />
     </main>
   );
