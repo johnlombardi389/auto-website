fcd78789fc91c882a6296c5ac28afce5937bda1a
johnlombardi389 <lombardij38@gmail.com>
2023-11-21 16:22:45 -0500

    Added text to intro section headline and styled image to move below text on smaller screens

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,18 +3,28 @@ import introImg from "../assets/introImg.jpg";
 
 const Intro = () => {
   return (
-    <section id="intro" className="flex items-center justify-center p-4">
+    <section
+      id="intro"
+      className="flex flex-col-reverse md:flex-row items-center justify-center p-4"
+    >
       {/* Left side with image */}
-      <div className="w-1/2">
+      <div className="w-full md:w-1/2 mb-4 md:mb-0">
         <img src={introImg} alt="Introduction" className="w-full h-auto" />
       </div>
 
       {/* Right side with text */}
-      <div className="w-1/2 px-8">
+      <div className="w-full md:w-1/2 px-8">
         <div className="text-center">
-          <h1 className="text-4xl font-bold mb-4">Introduction Heading</h1>
-          <p className="text-lg">
-            Smaller text underneath the heading goes here.
+          <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-4">
+            Revitalize Your Ride with Empire Auto Repair – Where Excellence
+            Drives Every Service!
+          </h1>
+          <p className="text-base md:text-lg lg:text-xl">
+            Our team of skilled technicians is committed to delivering top-notch
+            automotive solutions, from routine maintenance to complex repairs.
+            With cutting-edge diagnostics and a passion for precision, we ensure
+            your car not only runs smoothly but thrives on the road. Trust
+            Empire Auto Repair – where your journey meets excellence!
           </p>
         </div>
       </div>
