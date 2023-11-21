74f29b8be56c639a22272e647fa4362a4cf4fdfa
johnlombardi389 <lombardij38@gmail.com>
2023-11-21 15:30:52 -0500

    Added services titles and descriptions

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,42 +1,61 @@
 const Services = () => {
+  const servicesData = [
+    {
+      title: "Oil Change",
+      description:
+        "Regular oil changes help maintain engine health and improve fuel efficiency.",
+    },
+    {
+      title: "Brake Repair",
+      description:
+        "We provide brake inspection, repair, and replacement services to ensure your safety on the road.",
+    },
+    {
+      title: "Tire Rotation",
+      description:
+        "Regular tire rotations promote even tire wear and extend the lifespan of your tires.",
+    },
+    {
+      title: "Engine Diagnostics",
+      description:
+        "Our advanced diagnostics help identify and address issues affecting your vehicle's performance.",
+    },
+    {
+      title: "Battery Replacement",
+      description:
+        "If your car battery is weak or dead, we offer professional battery replacement services.",
+    },
+    {
+      title: "Scheduled Maintenance",
+      description:
+        "Stay on top of your vehicle's maintenance schedule with our comprehensive scheduled maintenance services.",
+    },
+  ];
+
   return (
     <section
       id="services"
       className="bg-gray-100 border-t border-gray-200 border-b p-12 text-center"
     >
-      <h3 className="text-2xl font-bold mb-4">What services do we provide?</h3>
-      <p className="text-gray-700 mb-8">Explore our range of services below.</p>
+      <h3 className="text-2xl font-bold mb-4 text-gray-800">
+        What services do we provide?
+      </h3>
+      <p className="text-gray-600 mb-8">Explore our range of services below.</p>
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8">
-        {/* Service Box 1 */}
-        <div className="bg-white p-4 text-center border border-gray-300">
-          <p>Service 1</p>
-        </div>
-
-        {/* Service Box 2 */}
-        <div className="bg-white p-4 text-center border border-gray-300">
-          <p>Service 2</p>
-        </div>
-
-        {/* Service Box 3 */}
-        <div className="bg-white p-4 text-center border border-gray-300">
-          <p>Service 3</p>
-        </div>
-
-        {/* Service Box 4 */}
-        <div className="bg-white p-4 text-center border border-gray-300">
-          <p>Service 4</p>
-        </div>
-
-        {/* Service Box 5 */}
-        <div className="bg-white p-4 text-center border border-gray-300">
-          <p>Service 5</p>
-        </div>
-
-        {/* Service Box 6 */}
-        <div className="bg-white p-4 text-center border border-gray-300">
-          <p>Service 6</p>
-        </div>
+        {servicesData.map((service, index) => (
+          <div
+            key={index}
+            className="bg-white p-4 text-center border border-gray-300"
+          >
+            <h4 className="text-sm md:text-lg lg:text-xl font-bold mb-4 text-gray-800">
+              {service.title}
+            </h4>
+            <p className="text-xs md:text-sm lg:text-base text-gray-700">
+              {service.description}
+            </p>
+          </div>
+        ))}
       </div>
     </section>
   );
